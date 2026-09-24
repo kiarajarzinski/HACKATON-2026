f9ae6314ee76b28845ff16892644ba29884ba62b
RamzLu <luanaabigail168@gmail.com>
2026-09-24 19:25:51 -0300

    se movio nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/auth/Navbar';
 // TODO: Crear estos componentes luego en la carpeta pages/
 // import { RegisterConsumidor } from './pages/RegisterConsumidor';
 // import { RegisterProductor } from './pages/RegisterProductor';
--- frontend/src/components/auth/Navbar.jsx
+++ frontend/src/components/auth/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { RoleModal } from '../components/auth/RoleModal';
+import { RoleModal } from './RoleModal';
 
 export const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
